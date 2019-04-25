db72dc6038b4da2baa7c58015e28dc4af5e4301a
lucas2502 <lucas2502@live.com>
2019-04-24 22:15:42 -0300

    Valores, Tipos e Operadores

---
+++ README.md
@@ -0,0 +1,41 @@
+# enloquence
+JavaScript Eloquente - 2ª edição
+
+Objetivo: 
+ - Fazer um capitulo/Dia com intuito de praticar JavaScript
+
+
+# Conteúdo do Livro
+0. Introdução
+1. Valores, Tipos e Operadores - (Parte 1: Linguagem)
+2. Estrutura do Programa
+3. Funções
+4. Estrutura de Dados: Objeto e Array
+5. Funções de Ordem Superior
+6. A Vida Secreta dos Objetos
+7. Prática: Vida Eletrônica
+8. Erros e Manipulação de Erros
+9. Expressões Regulares
+10. Módulos
+11. Prática: A Linguagem de Programação
+12. JavaScript e o Navegador - (Parte 2: Navegador)
+13. O Document Object Model
+14. Manipulando Eventos
+15. Projeto: Plataforma de Jogo
+16. Desenhando no Canvas
+17. HTTP
+18. Formulários e Campos de Formulários
+19. Projeto: Um Programa de Pintura
+20. Node.js - (Parte 3: Node.js)
+21. Projeto: Website de Compartilhamento de Habilidades
+
+
+# Referência
+<a>https://github.com/braziljs/eloquente-javascript</a>
+
+# Informações Importantes
+- Autor: Marijn Haverbeke
+- Versão original deste livro.
+- Licenciado sob a licença Creative Commons attribution-noncommercial.
+
+Todo código neste livro também pode ser considerado sob a licença MIT.
---
+++ index.js
@@ -0,0 +1,34 @@
+//Operadores Unários
+console.log("typeof 4.5 = ", typeof 4.5);
+console.log('typeof "x" = ', typeof "x");
+console.log("(- (10 - 2)) = " - (10 - 2));
+
+//Valores Booleanos"
+console.log("Valores Booleanos");
+console.log("3 > 2 = ", 3 > 2);
+console.log("3 < 2 = ", 3 < 2);
+console.log('"Aardvark" < "Zoroaster=" = ', "Aardvark" < "Zoroaster");
+console.log('"Itchy" != "Scratchy" = ', "Itchy" != "Scratchy" );
+console.log("NaN == NaN = ", NaN == NaN);
+
+//Operadores Lógicos
+console.log("Operadores Lógicos");
+console.log("true && false = ", true && false);
+console.log("true && true = ", true && true);
+console.log("false || true = ", false || true);
+console.log("false || false = ", false || false);
+
+//Conversão Automática de Tipo
+console.log("Conversão Automática de Tipo");
+console.log("8 * null = ", 8 * null);
+console.log('"5" - 1 = ', "5" - 1);
+console.log('"5" + 1 = ', "5" + 1);
+console.log('"five" * 2 = ', "five" * 2);
+console.log("false == 0 = ", false == 0);
+console.log("null == undefined = ", null == undefined);
+console.log("null == 0 = ", null == 0);
+
+//O Curto-Circuito de && e ||
+console.log("O Curto-Circuito de && e ||")
+console.log('null || "user" = ', null || "user");
+console.log('"Karl" || "user" = ', "Karl" || "user")
